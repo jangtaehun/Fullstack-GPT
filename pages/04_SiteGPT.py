38b9a4c0f83fa520e2595a9cb7521d823a81f662
Jangtaehun <jangth0056@naver.com>
2024-04-05 00:48:22 +0900

    #5 review

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,8 +1,55 @@
 import streamlit as st
 from langchain.document_loaders import AsyncChromiumLoader, SitemapLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 # https://openai.com/sitemap.xml#####
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    답변할 수 없다면 아무말이나 지어내지말고 모른다고 하세요.
+    그리고 각 답변을 0부터 5까지의 점수로 평가해주세요.
+    0점은 사용자에게 쓸모없음, 5점은 사용자에게 매우 유용함을 의미합니다.
+    사용자 question에 대한 예제입니다.
+    
+    Make sure to inclide the answer's score.
+    
+    Context: {context}
+    
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {"question": question, "context": doc.page_content}
+        )
+        answers.append(result.content)
+    st.write(answers)
 
 
 def parse_page(soup):
@@ -38,7 +85,8 @@ def load_website(url):
     )
     loader.requests_per_second = 5  # 속도는 느려지지만 차단은 안 당한다
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 
 st.set_page_config(
@@ -74,5 +122,11 @@ if url:
         with st.sidebar:
             st.error("Please write down a Sitemap url")
     else:
-        docs = load_website(url)
-        st.write(docs)
+        retriever = load_website(url)
+        # chain 2개: 1. 모든 개별 document에 대한 답변 생성 및 채점, 2. 모든 답변을 가진 마지막 시점에 실행 -> 점수가 제일 높고, 가장 최신 정보를 담고 있는 답변 고르기
+        chain = {"docs": retriever, "question": RunnablePassthrough()} | RunnableLambda(
+            get_answers
+        )
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision")
+        # 질문이 retriever로 전달 -> docs 반환 -> RunnablePassthrough는 question 값으로 교체된다.
+        # dictionary가 get_answers function의 입력값으로 전달되고, 그 function은 dictionary에서 documents와 question 값을 추출
