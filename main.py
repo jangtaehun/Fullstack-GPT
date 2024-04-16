f2e6e45508d246eaa8ef595f4e1fdc3078e3d206
Jangtaehun <jangth0056@naver.com>
2024-04-17 01:28:19 +0900

    #11.1

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import tiktoken
+import subprocess
 
-print(tiktoken)
+
+def extract_audio_from_video(video_path, audio_path):
+    command = ["/opt/homebrew/bin/ffmpeg", "-i", video_path, "-vn", audio_path]
+    subprocess.run(command)
+
+
+extract_audio_from_video("files/zzoneddeck.mp4", "files/audio.mp3")
